475f25d3b92349feceb53c5ad7d87d239d448b42
Mohit <mohitmore7029@gmail.com>
2021-08-24 18:58:09 +0530

    Revert "one line added"

---
+++ simple_calculator.py
@@ -0,0 +1,23 @@
+
+# inputs from user kadun
+no1 = float(input("Enter first number: "))
+no2 = float(input("Enter second number: "))
+# choise operation
+print("Operation: +, -, *, /")
+select = input("Select operations: ")
+# add two numbers
+if select == "+":
+ print(no1 + no2)
+# sub two numbers
+elif select == "-":
+ print(no1 - no2)
+# multiply two numbers
+elif select == "*":
+ print(no1 * no2)
+# divid two numbers
+elif select == "/":
+ print( no1 / no2)
+
+#my first mini project
+#mohit deeepak more
+# third day of course
